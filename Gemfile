74ad185b923524a37eb3317e52a3f607ec983397
VvDPzZ <vvdpzz@gmail.com>
2011-08-03 11:09:29 +0800

    init commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+gem 'mysql2', '~> 0.2.6'
+gem 'hiredis'
+gem 'redis'
+gem 'SystemTimer'
+gem 'devise'
+gem 'json'
+gem 'jquery-rails'
+gem 'pusher'
+gem 'uuidtools'
+gem 'resque'
+gem 'multi_json'
+
+gem 'capistrano'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Octopus::Application.config.secret_token = '0c4f44e77e43daff8d2264eb2cd75c0f427a1166d8be4f4ca226376b6d5d5c1b8dba026ff6ca688e27dccfe8ab39dc0675d2e10083faf5816247a3ac98d850ea'
