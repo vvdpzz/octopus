ec2ab0661bb09df760fcfe70e98653608ae47958
VvDPzZ <vvdpzz@gmail.com>
2011-08-03 12:55:37 +0800

    添加了模型

---
+++ db/migrate/20110803045004_create_notifications.rb
@@ -0,0 +1,13 @@
+class CreateNotifications < ActiveRecord::Migration
+  def self.up
+    create_table :notifications do |t|
+      t.references :user
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :notifications
+  end
+end
